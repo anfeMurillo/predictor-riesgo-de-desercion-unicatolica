790eb079fbccb2bd14b5f7bc860bfab835d42020
anfeMurillo <anfehumu2.0@hotmail.com>
2025-07-28 15:39:19 -0500

    Finish M5

---
+++ src/data/modulo_5.sql
@@ -0,0 +1,11 @@
+-- [Modulo 5]
+-- [Liberacion de tablas no necesarias]
+
+DROP VIEW temporal_estudiantes , temporal_periodos;
+
+DROP TABLE
+    in_process.creditos_por_periodo ,
+    in_process.promedio_y_creditos,
+    in_process.creditos_por_periodo,
+    in_process.historial_promedio_creditos
+    in_process.promedio;
